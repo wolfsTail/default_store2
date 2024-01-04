1fc16209d28f70529aafb58266eeb6783da092a8
DRomanov <lukaviy2la@gmail.com>
2024-01-04 09:46:49 +0300

    Add a main app logc

---
+++ main/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {}
+    context.update(
+        {"title": "Default Store - Главная", "content": "Default shop blank 2.0"}
+    )
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {}
+    context.update(
+        {
+            "title": "Default Store - О нас",
+            "content": "Коротко о нас!",
+            "text_on_page": "Заглушка информацию о сайте.",
+        }
+    )
+    return render(request, "main/about.html", context)
+
+
+def pay_and_delivery(request):
+    context = {}
+    context.update(
+        {
+            "title": "Default Store - Оплата и доставка",
+            "content": "Оплата и доставка",
+            "text_on_page": "Заглушка о способах оплаты и доставки.",
+        }
+    )
+    return render(request, "main/pay_and_delivery.html", context)
+
+
+def information(request):
+    context = {}
+    context.update(
+        {
+            "title": "Default Store - Контактная информация",
+            "content": "Контактная информация",
+            "text_on_page": "Заглушка для страницы информации о сайте.",
+        }
+    )
+    return render(request, "main/information.html", context)
